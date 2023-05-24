6dda80edd4c8cc7890f46bbfc02d8b12b7eac43e
ilukashevich <luiryna322@gmail.com>
2023-05-25 00:49:30 +0200

    fix in test

--- src/test/java/com/example/EFXjavatesttask/EfxJavaTestTaskApplicationTests.java
+++ src/test/java/com/example/EFXjavatesttask/EfxJavaTestTaskApplicationTests.java
@@ -22,7 +22,7 @@ class EfxJavaTestTaskApplicationTests {
     @Test
     void lastFedPriceTest() {
         Price expectedPrice = new Price();
-        expectedPrice.setId(106);
+        expectedPrice.setId(111);
         expectedPrice.setInstrument("EUR/USD");
         expectedPrice.setBid(1.0989);
         expectedPrice.setAsk(1.2012);
@@ -31,7 +31,7 @@ class EfxJavaTestTaskApplicationTests {
                 "108,GBP/USD, 1.2500,1.2560,01-06-2020 12:01:02:002\n" +
                 "109,GBP/USD, 1.2499,1.2561,01-06-2020 12:01:02:100\n" +
                 "110,EUR/JPY, 119.61,119.91,01-06-2020 12:01:02:110\n" +
-                "106,EUR/USD, 1.1000,1.2000,01-06-2020 12:01:03:001\n");
+                "111,EUR/USD, 1.1000,1.2000,01-06-2020 12:01:03:001\n");
         Price actualPrice = priceFeedProcessor.getLastPrice();
         assertEquals(expectedPrice, actualPrice);
     }
